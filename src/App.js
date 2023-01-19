6a573d53f34535b65fd6cb36eae5857da49a549c
francisco.nobre <franciscodiegonobre@gmail.com>
2023-01-19 12:30:30 +0100

    ejercicio-3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Escena from "./components/escena/Escena"
 import txt from "./data.json"
+import { StoryStyled } from "./components/escena/Escena.style";
 
 function App() {
-  const stories = txt.map(item => <Escena story={item}/>)
-  console.log(stories)
+  const stories = txt.map((item) =>  <StoryStyled><Escena story={item}/></StoryStyled>)
   
   return (
     <div>
-      {stories}  
+      {stories}
     </div>
-
+  
   );
 }
 
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,11 +1,5 @@
-/* export default function Escena(props) {
-    return <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>
-} */
-
 export default function Escena(props) {
-    return (
-        <div>
-            <p>{props.story}</p>
-        </div>
-    )
+  return <>{props.story}</>;
 }
+
+/* const Escena = (props) => <>{props.story}</>; */
